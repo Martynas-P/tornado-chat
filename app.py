e290510a569c1ca8e0a8e7eae7b2b001017076e9
Martynas Puronas <martynas.puronas@gmail.com>
2014-12-28 22:23:44 +0000

    Implemented basic functionality for a web based chat.

--- app.py
+++ app.py
@@ -1,14 +1,63 @@
+import json
+
 import tornado.ioloop
 import tornado.web
+import tornado.websocket
+
+usernames = set()
+listeners = set()
+id_counter = 0
+
 
 class AppHandler(tornado.web.RequestHandler):
 
     def get(self):
-        self.write('Hello, World!')
+        self.render('main.html')
 
-app = tornado.web.Application([
-    (r'/', AppHandler),
-])
+
+class ChatSocketHandler(tornado.websocket.WebSocketHandler):
+
+    def __init__(self, application, request, **kwargs):
+        super().__init__(application, request, **kwargs)
+        self.username = ''
+
+    def open(self):
+        global id_counter
+
+        listeners.add(self)
+
+        id_counter += 1
+        new_username = 'user' + str(id_counter)
+
+        usernames.add(new_username)
+        self.username = new_username
+
+        message = {'username': new_username}
+        self.write_message(json.dumps(message))
+
+    def on_message(self, message):
+        message_dict = {
+            'from': self.username,
+            'message': message
+        }
+
+        for listener in listeners:
+            listener.write_message(json.dumps(message_dict))
+
+    def on_close(self):
+        usernames.discard(self.username)
+        listeners.discard(self)
+
+
+app = tornado.web.Application(
+    handlers=[
+        (r'/', AppHandler),
+        (r'/chat', ChatSocketHandler)
+    ],
+    template_path='templates/',
+    static_path='static/',
+    autoreload=True
+)
 
 if __name__ == '__main__':
     app.listen(8000)
---
+++ static/app.js
@@ -0,0 +1,33 @@
+angular.module('chatApp', [])
+    .controller('mainController', function($scope){
+        $scope.username = '';
+
+        $scope.messages = [];
+
+        $scope.newMessage = null;
+
+        var socket = new WebSocket('ws://localhost:8000/chat');
+
+        socket.onmessage = function onMessage(data) {
+            var message = JSON.parse(data.data);
+
+            if (message.username) {
+                $scope.username = message.username;
+            } else if (message.from &&  message.message) {
+                $scope.messages.push(message);
+            }
+
+            $scope.$apply();
+        };
+
+        $scope.sendMessage = function sendMessage() {
+            console.log('clicked');
+            console.log($scope.newMessage);
+
+            if ($scope.newMessage) {
+                socket.send($scope.newMessage);
+            }
+
+            $scope.newMessage = '';
+        }
+    });
